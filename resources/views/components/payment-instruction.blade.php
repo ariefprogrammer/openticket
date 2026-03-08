fdbd936ac274dfdc809088192231c63bbdf2ed28
Arief Programmer <macbook@Macbooks-MacBook-Pro.local>
2026-03-08 11:01:30 +0700

    memperbaiki info pembayaran

--- resources/views/components/payment-instruction.blade.php
+++ resources/views/components/payment-instruction.blade.php
@@ -2,8 +2,8 @@
     <div class="p-3 bg-gray-100 dark:bg-gray-800 rounded-lg border-l-4 border-primary-500">
         <p class="font-bold text-gray-900 dark:text-white mb-1">Daftar Channel Pembayaran:</p>
         <ul class="list-disc list-inside">
-            <li><strong>Bank Mandiri :</strong> 1241231231</li>
-            <li><strong>Dana :</strong> 088219918654</li>
+            <li><strong>Bank Mandiri :</strong> 1240010211168 (a.n. Arief Hidayat)</li>
+            <li><strong>Dana :</strong> 088219918654 (a.n. Arief Hidayat)</li>
         </ul>
     </div>
     <p class="mt-2 italic text-primary-600">
